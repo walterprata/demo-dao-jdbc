c1ebb894a8d6b1de2064b5ec956023d7c3f51c11
walterprata <47155066+walterprata@users.noreply.github.com>
2020-01-03 10:20:55 -0300

    find by Id implemetation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,15 @@
 package application;
 
-import java.util.Date;
-
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Books");
-		Seller seller = new Seller(21, "bob", "bib@hdsusah", new Date(), 3000.0, obj);
-		System.out.println(seller );
+
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Seller seller = sellerDao.findById(3);
+		System.out.println(seller);
 	}
 }
