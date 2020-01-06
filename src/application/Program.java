4b009d0822221d07fe7c0fb13be586f6b66c7008
walterprata <47155066+walterprata@users.noreply.github.com>
2020-01-06 16:39:23 -0300

    Insert Implemetation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -27,6 +28,12 @@ public class Program {
 		list = sellerDao.findAll();
 		for( Seller obj : list) {
 			System.out.println(obj);
+			
 		}
+		
+		System.out.println("=== TEST 4: seller insert =====");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department );
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted: New id = " + newSeller.getId());
 	}
 }
